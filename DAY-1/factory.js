2b0db55f36c75bb1fa9f2045c5f3553b2a372b15
Tausifmanjar <tausifmanjar@gmial.com>
2024-09-20 11:43:52 +0530

    some updates on this assignment

--- DAY-1/factory.js
+++ DAY-1/factory.js
@@ -2,27 +2,27 @@
 
 //PROBLEM 1
 
-function iPhone1(ASIN, color, display, camera){
-      let obj = {};
+function iPhone1(ASIN, color, display, camera) {
+  let obj = {};
 
-      obj.ASIN = ASIN;
-      obj.color = color;
-      obj.display = display;
-      obj.camera = camera;
+  obj.ASIN = ASIN;
+  obj.color = color;
+  obj.display = display;
+  obj.camera = camera;
 
-      obj.dial = function(){
-        console.log("tring.... tring....");
-        
-      }
-      obj.sendMessage  = function(){
-        console.log("Sending message");
-        
-      }
-      obj.cameraClick = function(){
-        console.log("Camera clicked");
-        
-      }
-      return obj;
+  obj.dial = function () {
+    console.log("tring.... tring....");
+
+  }
+  obj.sendMessage = function () {
+    console.log("Sending message");
+
+  }
+  obj.cameraClick = function () {
+    console.log("Camera clicked");
+
+  }
+  return obj;
 }
 
 
@@ -41,12 +41,12 @@ function iPhone1(ASIN, color, display, camera){
 //   console.log(i1.color); // "B09X67JBQV"
 //   console.log(i1.display); // 7.8
 //   console.log(i1.camera); // "IOS"
-  
+
 // //   ---- methods
 //   console.log(i1.dial()); // "tring.. tring..."
 //   console.log(i1.sendMessage()); // "Sending message..."
 //   console.log(i1.cameraClick()); // "Camera clicked"
-  
+
 
 
 //PROBLEM 2
@@ -61,22 +61,22 @@ function iPhone1(ASIN, color, display, camera){
 
 //     obj.dial = function(){
 //       console.log("tring.... tring....");
-      
+
 //     }
 //     obj.sendMessage  = function(){
 //       console.log("Sending message");
-      
+
 //     }
 //     obj.cameraClick = function(){
 //       console.log("Camera clicked");
-      
+
 //     }
 //     obj.connectBluetooth = function(){
 //         console.log("Bluetooth connected");
-        
+
 //     }
 //     return obj;
-    
+
 // }
 
 
@@ -91,27 +91,27 @@ function iPhone1(ASIN, color, display, camera){
 //       width,
 //       cameraSpec,
 //       osVersion,
-      
+
 //       // Methods
 //       dial() {
 //         return "tring.. tring...";
 //       },
-      
+
 //       sendMessage() {
 //         return "Sending message...";
 //       },
-      
+
 //       cameraClick() {
 //         return "Camera clicked";
 //       },
-      
+
 //       connectBluetooth() {
 //         return "Bluetooth connected successfully...";
 //       }
 //     };
 //   }
-  
-  
+
+
 // let i2 = iPhone2(
 //     1,
 //     "B09X67JBQV",
@@ -125,13 +125,13 @@ function iPhone1(ASIN, color, display, camera){
 //   console.log(i2.display); // 7.8
 //   console.log(i2.camera); // "IOS"
 //   console.log(i2.bluetooth); // "128GB"
-  
+
 // //   ---- methods
 //   console.log(i2.dial()); // "tring.. tring..."
 //   console.log(i2.sendMessage()); // "Sending message..."
 //   console.log(i2.cameraClick()); // "Camera clicked"
 //   console.log(i2.connectBluetooth()); //"Bluetooth connected successfully..."
-  
+
 
 
 
@@ -144,19 +144,19 @@ function iPhone1(ASIN, color, display, camera){
 //     this.display = display
 //     this.camera = camera  
 //     this.bluetooth = bluetooth
-    
+
 //     this.dial = function() {
 //       return "tring.. tring...";
 //     };
-  
+
 //     this.sendMessage = function() {
 //       return "Sending message...";
 //     };
-  
+
 //     this.cameraClick = function() {
 //       return "Camera clicked";
 //     };
-  
+
 //     this.connectBluetooth = function() {
 //       return "Bluetooth connected successfully...";
 //     };
@@ -190,55 +190,131 @@ function iPhone1(ASIN, color, display, camera){
 
 
 
-   class Iphone4{
-    constructor(ASIN, color, display, camera, bluetooth) {
-        this.ASIN = ASIN 
-        this.color = color 
-        this.display = display
-        this.camera = camera  
-        this.bluetooth = bluetooth
-      }
-        
-        dial() {
-          return "tring.. tring...";
-        };
-      
-        sendMessage() {
-          return "Sending message...";
-        };
-      
-        cameraClick() {
-          return "Camera clicked";
-        };
-      
-        connectBluetooth() {
-          return "Bluetooth connected successfully...";
-        };
-    
-   }
-let i4 = new Iphone4(
-    1,
-    "B09X67JBQV",
-    7.8,
-    "IOS",
-    "128mb",
-    "Gray",
-    "90mm",
-    "2.0 MP",
-    "5.1"
-  );
-//   ---- properties
-  console.log(i4.ASIN); // 1
-  console.log(i4.color); // B09X67JBQV
-  console.log(i4.display); // 7.8
-  console.log(i4.camera); // IOS
-  console.log(i4.bluetooth); // 128mb
-  
-//   ---- methods
-  console.log(i4.dial()); // "tring.. tring..."
-  console.log(i4.sendMessage()); // "Sending message..."
-  console.log(i4.cameraClick()); // "Camera clicked"
-  console.log(i4.connectBluetooth()); // "Bluetooth connected successfully..."
-  
+//    class Iphone4{
+//     constructor(ASIN, color, display, camera, bluetooth) {
+//         this.ASIN = ASIN 
+//         this.color = color 
+//         this.display = display
+//         this.camera = camera  
+//         this.bluetooth = bluetooth
+//       }
+
+//         dial() {
+//           return "tring.. tring...";
+//         };
+
+//         sendMessage() {
+//           return "Sending message...";
+//         };
+
+//         cameraClick() {
+//           return "Camera clicked";
+//         };
+
+//         connectBluetooth() {
+//           return "Bluetooth connected successfully...";
+//         };
+
+//    }
+// let i4 = new Iphone4(
+//     1,
+//     "B09X67JBQV",
+//     7.8,
+//     "IOS",
+//     "128mb",
+//     "Gray",
+//     "90mm",
+//     "2.0 MP",
+//     "5.1"
+//   );
+// //   ---- properties
+//   console.log(i4.ASIN); // 1
+//   console.log(i4.color); // B09X67JBQV
+//   console.log(i4.display); // 7.8
+//   console.log(i4.camera); // IOS
+//   console.log(i4.bluetooth); // 128mb
+
+// //   ---- methods
+//   console.log(i4.dial()); // "tring.. tring..."
+//   console.log(i4.sendMessage()); // "Sending message..."
+//   console.log(i4.cameraClick()); // "Camera clicked"
+//   console.log(i4.connectBluetooth()); // "Bluetooth connected successfully..."
 
 
+//QUESTION NO 9;
+//FACTORY FUNCTION PROBLEM
+
+
+function animal(noOfLegs, vegeterian) {
+
+  let obj = {}
+  obj.noOfLegs = noOfLegs;
+  obj.vegeterian = vegeterian;
+
+  obj.eat = function () {
+     return "eating..."
+
+  }
+  obj.greet = function () {
+   return `Hi, I have ${noOfLegs} legs.`
+
+  }
+  return obj;
+}
+
+
+
+
+
+
+// let a1 = animal(4, true);
+// console.log(a1.eat()); // eating...
+// console.log(a1.greet()); // Hi, I have 4 legs.
+
+
+
+//SAME PROBLEM IN CONSTRUCTOR FUNCTION 
+  
+function AnimalCF(noOfLegs, vegeterian) {
+  this.noOfLegs = noOfLegs;
+  this.vegeterian = vegeterian;
+
+  this.eat = function () {
+     return "eating..."
+
+  }
+  this.greet = function () {
+   return `Hi, I have ${this.noOfLegs} legs.`
+
+  }
+}
+
+
+
+
+// let animalCF = new AnimalCF(4, true);
+// console.log(animalCF.eat()); // eating...
+// console.log(animalCF.greet()); // Hi, I have 4 legs.
+
+
+
+//SAME PROBLEM IN ES6 CLASS
+class AnimalES6{
+  constructor(noOfLegs, vegeterian) {
+    this.noOfLegs = noOfLegs;
+    this.vegeterian = vegeterian;
+  }
+  
+    eat() {
+       return `eating...`
+  
+    }
+    greet() {
+     return `Hi, I have ${this.noOfLegs} legs.`
+  
+    }
+}
+
+let animalES6 = new AnimalES6(4, true);
+console.log(animalES6.eat()); // eating...
+console.log(animalES6.greet()); // Hi, I have 4 legs.
