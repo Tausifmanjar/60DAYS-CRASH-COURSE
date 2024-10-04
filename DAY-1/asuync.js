57eebb3939131ecabdbdd483c5be5b2217895f78
Tausifmanjar <tausifmanjar@gmial.com>
2024-10-04 20:20:33 +0530

    fetch assignmetns

---
+++ DAY-1/asuync.js
@@ -0,0 +1,12 @@
+function calculate(){
+   let operation = (num1,num2) => {
+    console.log(callback(19,3));
+   }
+}
+
+
+calculate()
+operation(num1,num2,callback = (num1,num2) => {
+    return num1 + num2
+})
+
